245b2c0c66cf642e15df1521436ed65648a11796
Abhimanyu Gupta <abhimanyugupta7880@gmail.com>
2026-05-20 10:51:39 +0000

    variable state

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail ="abhi0905st@gmail.com";
+var accountPassword="12345";
+accountCity = "Jaipur";
+
+let accountState;
+
+// accountId=23; not allowed
+console.log(accountId);
+accountEmail="abhi090u@gmail.com";
+accountPassword="123";
+accountCity="Delhi";
+console.table([accountId,accountEmail, accountPassword,accountCity, accountState]);
